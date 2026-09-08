aaac4eb01a2cc267755d6a1bdcfbcd18c826b97f
dustinginn <dustinjginn@gmail.com>
2026-09-07 23:25:17 -0700

    Prepare PhysiqueOS Sandbox build 15

--- ios/PhysiqueOSTests/TrainingLoggerTests.swift
+++ ios/PhysiqueOSTests/TrainingLoggerTests.swift
@@ -474,11 +474,11 @@ final class TrainingLoggerTests: XCTestCase {
         XCTAssertTrue(InteractivePopGesturePolicy.shouldEnable(viewControllerCount: 2))
     }
 
-    func testAppDeclaresExemptEncryptionAndBuildFourteenInSourceControlledConfiguration() throws {
+    func testAppDeclaresExemptEncryptionAndBuildFifteenInSourceControlledConfiguration() throws {
         let usesNonExemptEncryption = try XCTUnwrap(Bundle.main.object(forInfoDictionaryKey: "ITSAppUsesNonExemptEncryption") as? Bool)
         XCTAssertFalse(usesNonExemptEncryption)
         XCTAssertEqual(Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String, "1.0")
-        XCTAssertEqual(Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as? String, "14")
+        XCTAssertEqual(Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as? String, "15")
         XCTAssertEqual(Bundle.main.bundleIdentifier, "com.physiqueos.native.dev")
     }
 }
