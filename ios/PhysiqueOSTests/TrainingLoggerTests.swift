757294441e0ca6a9986119d55a21745223dd6d92
dustinginn <dustinjginn@gmail.com>
2026-09-15 11:53:41 -0700

    chore(ios): prepare Build 34 shipping metadata

--- ios/PhysiqueOSTests/TrainingLoggerTests.swift
+++ ios/PhysiqueOSTests/TrainingLoggerTests.swift
@@ -862,11 +862,11 @@ final class TrainingLoggerTests: XCTestCase {
         XCTAssertTrue(InteractivePopGesturePolicy.shouldEnable(viewControllerCount: 2))
     }
 
-    func testAppDeclaresExemptEncryptionAndBuildThirtyThreeInSourceControlledConfiguration() throws {
+    func testAppDeclaresExemptEncryptionAndBuildThirtyFourInSourceControlledConfiguration() throws {
         let usesNonExemptEncryption = try XCTUnwrap(Bundle.main.object(forInfoDictionaryKey: "ITSAppUsesNonExemptEncryption") as? Bool)
         XCTAssertFalse(usesNonExemptEncryption)
         XCTAssertEqual(Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String, "1.0")
-        XCTAssertEqual(Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as? String, "33")
+        XCTAssertEqual(Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as? String, "34")
         XCTAssertEqual(Bundle.main.bundleIdentifier, "com.physiqueos.native.dev")
     }
 }
